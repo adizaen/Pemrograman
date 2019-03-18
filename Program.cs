a1d704bb9d135c184f0fe4ae445bd2f7c49c6f56
Adi Zaenul Mustaqim <adi.mustaqim@students.amikom.ac.id>
2019-03-18 23:07:16 +0700

    C#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "C#";
+            Console.Write("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
